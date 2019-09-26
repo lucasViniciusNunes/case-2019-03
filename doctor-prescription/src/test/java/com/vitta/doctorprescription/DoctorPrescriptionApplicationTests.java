72dd48fe1a98aed88c46b2b2c895d7f2224ae9f6
Lucas Nunes <lucas.nunes@socialbank.com.br>
2019-09-26 08:50:42 -0300

    Created Spring boot project

---
+++ doctor-prescription/src/test/java/com/vitta/doctorprescription/DoctorPrescriptionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vitta.doctorprescription;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoctorPrescriptionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
